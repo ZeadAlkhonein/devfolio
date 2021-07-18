d73e53a4f2ec644d5f352fe6b1130bd481af2c6d
ZeadAlkhonein <ziyadalkhonein@gmail.com>
2021-07-18 17:35:25 +0300

    add personal info

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,73 +3,77 @@ module.exports = {
     // Site URL for when it goes live
     siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Ziyad Alkhonein',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Ziyad Alkhonein | Data Engineer`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: ``,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    author: `@ZiadAlkhonein`,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/ZeadAlkhonein`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: `https://www.linkedin.com/in/ziyad-alkhonein-329070135/`,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: `Data Engineer working to advance data-driven cultures in Saudi Arabia by integrating disparate data sources and empowering users to uncover key insights that tell a bigger story.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
-      {
-        name: 'Devfolio',
-        description:
-          'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-      {
-        name: 'ChromeExtensionKit',
-        description:
-          'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
-        link: 'https://chromeextensionkit.com/?ref=devfolio',
-      },
-      {
-        name: 'Another Cool Project',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
+      // {
+      //   name: 'Devfolio',
+      //   description:
+      //     'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
+      //   link: 'https://github.com/RyanFitzgerald/devfolio',
+      // },
+      // {
+      //   name: 'ChromeExtensionKit',
+      //   description:
+      //     'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
+      //   link: 'https://chromeextensionkit.com/?ref=devfolio',
+      // },
+      // {
+      //   name: 'Another Cool Project',
+      //   description:
+      //     'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
+      //   link: 'https://github.com/RyanFitzgerald/devfolio',
+      // },
     ],
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
       {
-        name: 'Acme Corp',
-        description: 'Full-Stack Developer, February 2020 - Present',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'SITE',
+        description: 'Data Engineer, Jan 2020 - Present',
+        //link: 'https://github.com/RyanFitzgerald/devfolio',
       },
       {
-        name: 'Globex Corp',
-        description: 'Full-Stack Developer, December 2017 - February 2020',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'STC',
+        description: 'Data-Mart Developer, Sep 2018 - Jan 2020',
+        //link: 'https://github.com/RyanFitzgerald/devfolio',
       },
       {
-        name: 'Hooli',
-        description: 'Full-Stack Developer, May 2015 - December 2017',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Confidintal',
+        description: 'Software Engineer, Jan 2018 - Sep 2018',
+        //link: 'https://github.com/RyanFitzgerald/devfolio',
       },
+      {
+        name: 'IBM',
+        description: 'Software Engineer Intern',
+      }
     ],
     // Optional: List your skills, they must have `name` and `description`.
     skills: [
       {
         name: 'Languages & Frameworks',
         description:
-          'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
+          'Python, TeraData SQL and PL/SQL, PostgreSQL, git, Django',
       },
       {
-        name: 'Databases',
-        description: 'MongoDB, PostreSQL, MySQL',
+        name: 'Technologies',
+        description: 'S3, ElasticStack, Nifi, Kafka',
       },
       {
         name: 'Other',
         description:
-          'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
+          'Docker, Data Analysis, ETL',
       },
     ],
   },
