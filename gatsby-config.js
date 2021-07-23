bacfbd0738c0a36bd247108874559c37beeef6a0
ZeadAlkhonein <ziyadalkhonein@gmail.com>
2021-07-23 21:27:04 +0300

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -131,7 +131,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: `ADD YOUR TRACKING ID HERE`, // Optional Google Analytics
+        trackingId: `G-HNFWW99V8L`, // Optional Google Analytics
       },
     },
     {
